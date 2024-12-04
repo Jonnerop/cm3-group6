59b3adfd5a4c4170defc201ba6514ea2934a6663
Jonne Roponen <Jonne.Roponen@metropolia.fi>
2024-12-04 16:29:33 +0200

    add authentication and route protection

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,23 +3,38 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const generateToken = (_id) => {
-    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
 };
 
 const signup = async (req, res) => {
-    const { name, username, password, phone_number,
-        gender, date_of_birth, membership_status, address,
-        profile_picture } = req.body;
+    const { name, 
+        username, 
+        password, 
+        phone_number,
+        gender, 
+        date_of_birth, 
+        membership_status, 
+        address,
+        profile_picture 
+    } = req.body;
     try {
-        if (!name || !username || !password ||
-            !phone_number || !gender || !date_of_birth ||
-            !membership_status || !address) {
+        if (!name || 
+            !username || 
+            !password ||
+            !phone_number || 
+            !gender || 
+            !date_of_birth ||
+            !membership_status || 
+            !address
+        ) {
             res.status(400).json({ message: "All fields are required" });
         }
 
-        const userExits = await User.findOne({ username });
-        if (userExits) {
-            res.status(400).json({ message: "User already exists" });
+        const userExists = await User.findOne({ username });
+
+        if (userExists) {
+            res.status(400);
+            throw new Error("User already exists");
         }
 
         const salt = await bcrypt.genSalt(13);
@@ -28,17 +43,50 @@ const signup = async (req, res) => {
         const user = await User.create({
             name, username, password: hashedPassword,
             phone_number, gender, date_of_birth, address,
-            membership_status, profile_picture });
+            membership_status, profile_picture
+        });
 
         if (user) {
             const token = generateToken(user._id);
             res.status(201).json({ username, token });
         } else {
-            res.status(400);
+            res.status(400).json({ message: "Invalid user data" });
             throw new Error('Invalid user data');
         }
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
+};
 
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        if (!username || !password) {
+            res.status(400).json({ message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ username });
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = generateToken(user._id);
+            res.status(200).json({ username, token });
+        } else {
+            res.status(400).json({ message: "Invalid username or password" });
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const getMe = async (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    signup,
+    login,
+    getMe,
 };
