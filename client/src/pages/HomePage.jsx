0154f4016a0a28f576e9f62739339ab34e8bd503
Goliathuzzzz <mlaakkonen@live.com>
2024-12-04 14:56:56 +0200

    Added styling to navbar and homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import JobListings from '../components/JobListings';
+import { useEffect, useState } from "react";
+import JobListings from "../components/JobListings";
 
 const HomePage = () => {
   const [jobs, setJobs] = useState([]);
@@ -8,7 +8,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const response = await fetch('/jobs');
+        const response = await fetch("/api/jobs");
         if (!response.ok) {
           throw new Error(`Could not fetch jobs, status: ${response.status}`);
         }
@@ -16,18 +16,24 @@ const HomePage = () => {
         setJobs(data);
         setIsPending(false);
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
         setIsPending(false);
       }
     };
     fetchJobs();
   }, [jobs]);
 
+  console.log(isPending);
+
   return (
-    <div id="homePage">
-      <h1>Job Listings</h1>
-      {isPending && <div>Loading...</div>}
-      {jobs && <JobListings jobs={jobs} />}
+    <div id="homePage" className="flex flex-col">
+      <div className="flex m-auto">
+        <h1>Job Listings</h1>
+      </div>
+      <div id="Jobs-container" className="flex flex-wrap justify-between">
+        {isPending && <div className="border">Loading...</div>}
+        {jobs && <JobListings jobs={jobs} />}
+      </div>
     </div>
   );
 };
